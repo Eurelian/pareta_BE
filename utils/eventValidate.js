c7ee572e3f556649ccdeaf95cebc8903bcf80ec6
Eurelian <44678944+Eurelian@users.noreply.github.com>
2020-07-02 16:00:21 +0200

    m

--- utils/eventValidate.js
+++ utils/eventValidate.js
@@ -1,11 +1,11 @@
 const Joi = require("@hapi/joi");
 
 const eventValidate = Joi.object({
-	name: Joi.string().min(8).max(255).required(),
-	date: Joi.required(),
-	age_group: Joi.required(),
-	description: Joi.string().min(70).max(255).required(),
-	size: Joi.number().required(),
+	// name: Joi.string().min(8).max(255).required(),
+	// date: Joi.required(),
+	// age_group: Joi.required(),
+	// description: Joi.string().min(70).max(255).required(),
+	// size: Joi.number().required(),
 });
 
 module.exports = eventValidate;
