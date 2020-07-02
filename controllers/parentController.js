d99c3032126276f9f9dd2d969ce1d66256bdc75d
Eurelian <44678944+Eurelian@users.noreply.github.com>
2020-07-02 16:08:19 +0200

    m

--- controllers/parentController.js
+++ controllers/parentController.js
@@ -228,7 +228,7 @@ exports.parent_event_create = async (req, res) => {
 		const user = await Parent.findById(_id);
 		const event = await new Event({
 			name: name,
-			location: geometry,
+			geometry: geometry,
 			date: date,
 			age_group: age_group,
 			description: description,
