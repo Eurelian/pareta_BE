82058e2e8620e42d14fc9299ec15b102f94c3239
Eurelian <44678944+Eurelian@users.noreply.github.com>
2020-07-02 17:21:34 +0200

    m

--- utils/loginValidate.js
+++ utils/loginValidate.js
@@ -1,8 +1,14 @@
 const Joi = require("@hapi/joi");
 
 const loginValidate = Joi.object({
-	email: Joi.string().email().required(),
-	password: Joi.string().min(8).max(255).required(),
+	email: Joi.string().email().required().messages({
+		"string.empty": "Please enter a registered email address",
+		"any.required": "Please enter a registered email address",
+	}),
+	password: Joi.string().min(8).max(255).required().messages({
+		"string.empty": "Please enter your password",
+		"any.required": "Please enter your password",
+	}),
 });
 
 module.exports = loginValidate;
