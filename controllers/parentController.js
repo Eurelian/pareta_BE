3702505a45cf3c912ca027f88b3513117b56a9e1
Eurelian <44678944+Eurelian@users.noreply.github.com>
2020-07-05 16:04:39 +0200

    m

--- controllers/parentController.js
+++ controllers/parentController.js
@@ -105,6 +105,7 @@ exports.favorite_parent = async (req, res) => {
 
 		if (sender.parents_favorite.includes(user._id))
 			return res.send("This parent is already in your Favorites List");
+		if (user._id == sender._id) return res.send("Can't message yourself");
 
 		sender.parents_favorite.push(user._id);
 		await sender.save();
