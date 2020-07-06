fe7a1e02a281cfd871713a6cad6856985859269c
Eurelian <44678944+Eurelian@users.noreply.github.com>
2020-07-06 09:38:30 +0200

    m

--- models/article.js
+++ models/article.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const articleSchema = new Schema({
-	title: { type: String, max: 85, text: true },
-	text: { type: String, text: true },
-	author: { type: Schema.Types.ObjectId, ref: "Parent", text: true },
-});
+const articleSchema = new Schema(
+	{
+		title: { type: String, max: 85, text: true },
+		text: { type: String, text: true },
+		author: { type: Schema.Types.ObjectId, ref: "Parent", text: true },
+	},
+	{ timestamps: true }
+);
 
 module.exports = mongoose.model("Article", articleSchema);
